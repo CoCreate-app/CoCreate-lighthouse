14d0653fad4af8ec38abba099138d61af03bed56
frank <frank@cocreate.app>
2021-04-21 18:09:48 +0000

    fix: One package for server and client

---
+++ src/client.js
@@ -0,0 +1,18 @@
+const CoCreateLightHouse = {
+    id: 'lighthouse',
+    actions: [
+        'getHtml',
+    ],
+
+    action_getHtml: function (element, data) {
+        let container = element.closest("form");
+        let formdData = CoCreate.api.getFormData('lighthouse', 'getHtml', container);
+        CoCreate.api.send('lighthouse', 'getHtml', formdData);
+    },
+}
+
+
+api.init({
+	name: CoCreateLightHouse.id, 
+	module:	CoCreateLightHouse,
+});
