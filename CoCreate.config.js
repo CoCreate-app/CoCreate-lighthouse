3d468d04f733a996daec59527fd4c6773ab508a5
frank pagan <frank@cocreate.app>
2021-09-14 03:55:40 +0000

    fix: add cocreate.app to domains

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/lighthouse/index.html",
                 "domains": [
+                    "cocreate.app",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/lighthouse",
