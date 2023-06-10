6aada78cf8376bab4e9c2b8e244ecd5b69663698
frankpagan <frank@cocreate.app>
2023-06-10 16:16:47 -0600

    fix: renamed hosts to host. the value can be a string or an array of strings

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -10,7 +10,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/lighthouse/index.html",
                 "src": "{{./docs/index.html}}",
-                "hosts": [
+                "host": [
                     "*",
                     "general.cocreate.app"
                 ],
